433edc2dc121220e3bb9b47bf20ca494416a8c21
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-21 21:33:29 +0300

    Добавлены страницы: разводящая, регистрация и вход; добавлена маршрутизация

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,18 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Intro from '../../pages/intro/intro';
+import SignUp from '../../pages/sign-up/sign-up';
+import SignIn from '../../pages/sign-in/sign-in';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'intro'} element={<Intro />}/>
+        <Route path={'signup'} element={<SignUp />}/>
+        <Route path={'signin'} element={<SignIn />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
