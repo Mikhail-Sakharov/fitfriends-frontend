68b7c489e8fa737be2642e4245095f83191ab93f
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-22 21:34:37 +0300

    Добавлены страницы: главная, опросники пользователя и тренера

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,6 +2,9 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Intro from '../../pages/intro/intro';
 import SignUp from '../../pages/sign-up/sign-up';
 import SignIn from '../../pages/sign-in/sign-in';
+import SignUpQuestionnaireCoach from '../../pages/sign-up-questionnaire-coach/sign-up-questionnaire-coach';
+import SignUpQuestionnaireUser from '../../pages/sign-up-questionnaire-user/sign-up-questionnaire-user';
+import Main from '../../pages/main/main';
 
 function App(): JSX.Element {
   return (
@@ -9,6 +12,9 @@ function App(): JSX.Element {
       <Routes>
         <Route path={'intro'} element={<Intro />}/>
         <Route path={'signup'} element={<SignUp />}/>
+        <Route path={'coach'} element={<SignUpQuestionnaireCoach />}/>
+        <Route path={'user'} element={<SignUpQuestionnaireUser />}/>
+        <Route path={'main'} element={<Main />}/>
         <Route path={'signin'} element={<SignIn />}/>
       </Routes>
     </BrowserRouter>
