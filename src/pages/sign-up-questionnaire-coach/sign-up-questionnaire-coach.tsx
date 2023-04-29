fca15d3b8a3f6b891abe27278535ab652788e4a1
Михаил Сахаров <sakharov_mikhail@hotmail.com>
2023-04-29 19:47:41 +0300

    Добавил валидацию файла сертификата и валидацию описания тренера

--- src/pages/sign-up-questionnaire-coach/sign-up-questionnaire-coach.tsx
+++ src/pages/sign-up-questionnaire-coach/sign-up-questionnaire-coach.tsx
@@ -1,27 +1,36 @@
 import {nanoid} from 'nanoid';
 import {TrainingType} from '../../types/training-type.enum';
-import {FormEvent, useEffect, useState} from 'react';
+import {ChangeEvent, FormEvent, useEffect, useState} from 'react';
 import {TrainingLevel} from '../../types/training-level.enum';
 
+export const CERTIFICATE_FILE_TYPES = ['jpg', 'pdf', 'png'];
+
 function SignUpQuestionnaireCoach(): JSX.Element {
+  const [certificate, setCertificate] = useState<File | null>(null);
   const [trainingTypes, setTrainingTypes] = useState<TrainingType[]>([]);
   const [trainingLevel, setTrainingLevel] = useState('');
+  const [description, setDescription] = useState('');
+  const [isReadyToTrain, setIsReadyToTrain] = useState(false);
 
   const [isTrainingTypesInputUsed, setIsTrainingTypesInputUsed] = useState(false);
   const [isTrainingLevelInputUsed, setIsTrainingLevelInputUsed] = useState(false);
+  const [descriptionInputUsed, setDescriptionInputUsed] = useState(false);
+  const [imageInputUsed, setImageInputUsed] = useState(false);
 
+  const [imageError, setImageError] = useState('Добавьте подтверждающий документ');
   const [trainingTypesError, setTrainingTypesError] = useState('Выберите типы тренировок');
   const [trainingLevelError, setTrainingLevelError] = useState('Выберите ваш уровень подготовки');
+  const [descriptionError, setDescriptionError] = useState('Заполните поле');
 
   const [isFormValid, setIsFormValid] = useState(false);
 
   useEffect(() => {
-    if (trainingTypesError || trainingLevelError) {
+    if (imageError || trainingTypesError || trainingLevelError || descriptionError) {
       setIsFormValid(false);
     } else {
       setIsFormValid(true);
     }
-  }, [trainingTypesError, trainingLevelError]);
+  }, [imageError, trainingTypesError, trainingLevelError, descriptionError]);
 
   const checkTrainingTypesNumber = (typesNumber: number) => {
     if (typesNumber < 1) {
@@ -58,16 +67,63 @@ function SignUpQuestionnaireCoach(): JSX.Element {
     setIsTrainingLevelInputUsed(true);
   };
 
+  const handleDescriptionInputChange = (evt: FormEvent<HTMLTextAreaElement>) => {
+    const value = evt.currentTarget.value;
+    setDescription(value);
+    if (value.length < 10 || value.length > 140) {
+      setDescriptionError('Длина описания 10 до 140 символов');
+      if (!value) {
+        setDescriptionError('Заполните поле');
+      }
+    } else {
+      setDescriptionError('');
+    }
+  };
+
+  const handleDescriptionInputFocus = () => {
+    setDescriptionInputUsed(true);
+  };
+
+  const handleCertificateFileInputChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const file = evt.currentTarget.files && evt.currentTarget.files[0];
+    const fileName = file ? file.name.toLowerCase() : '';
+    const matches = CERTIFICATE_FILE_TYPES.some((fileType) => fileName.endsWith(fileType));
+
+    if (matches && file) {
+      setCertificate(file);
+
+      setImageError('');
+    } else if (!matches && file) {
+      setImageError('Загрузите сюда файлы формата PDF, JPG или PNG');
+    } else {
+      setImageError('Добавьте подтверждающий документ');
+    }
+  };
+
   const handleSubmitButtonClick = (evt: FormEvent<HTMLButtonElement>) => {
     evt.preventDefault();
     if (isFormValid) {
       console.log({
+        certificate,
         trainingTypes,
-        trainingLevel
+        trainingLevel,
+        description,
+        isReadyToTrain
       });
+
+      /* if (certificate) {
+        const formData = new FormData();
+        formData.append('certificate', certificate, 'certificate.pdf');
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:5678/users/certificate');
+        xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDM1OTEyOTgzNTg3NWRiMWExYWVhZGUiLCJlbWFpbCI6ImpvaG5AcXdlLnF3ZSIsInVzZXJOYW1lIjoiSm9obiIsInVzZXJSb2xlIjoi0YLRgNC10L3QtdGAIiwiaWF0IjoxNjgyNzg2NDI0LCJleHAiOjE2ODI3ODczMjR9.SErnzcKniFvw-Q9VgUdIAImb-41V8KsLqMQA5j6AhTo');
+        xhr.send(formData);
+      } */
     }
     setIsTrainingTypesInputUsed(true);
     setIsTrainingLevelInputUsed(true);
+    setDescriptionInputUsed(true);
+    setImageInputUsed(true);
   };
 
   return (
@@ -156,27 +212,43 @@ function SignUpQuestionnaireCoach(): JSX.Element {
                     <div className="questionnaire-coach__block">
                       <span className="questionnaire-coach__legend">Ваши дипломы и сертификаты</span>
                       <div className="drag-and-drop questionnaire-coach__drag-and-drop">
-                        <label>
+                        <label className={`${imageInputUsed && imageError ? 'custom-input--error' : ''}`}>
                           <span className="drag-and-drop__label" tabIndex={0}>
                             Загрузите сюда файлы формата PDF, JPG или PNG
                             <svg width="20" height="20" aria-hidden="true">
                               <use xlinkHref="#icon-import"></use>
                             </svg>
                           </span>
-                          <input type="file" name="import" tabIndex={-1} accept=".pdf, .jpg, .png"/>
+                          <input
+                            onChange={handleCertificateFileInputChange}
+                            type="file" name="import" tabIndex={-1} accept=".pdf, .jpg, .png"
+                          />
+                          <span className="custom-input__error">
+                            {imageInputUsed && imageError}
+                          </span>
                         </label>
                       </div>
                     </div>
                     <div className="questionnaire-coach__block">
                       <span className="questionnaire-coach__legend">Расскажите о своём опыте, который мы сможем проверить</span>
                       <div className="custom-textarea questionnaire-coach__textarea">
-                        <label>
-                          <textarea name="description" placeholder=" "></textarea>
+                        <label className={`${descriptionInputUsed && descriptionError ? 'custom-input--error' : ''}`}>
+                          <textarea
+                            className={`${descriptionInputUsed && descriptionError ? 'custom-textarea__error' : ''}`}
+                            onFocus={handleDescriptionInputFocus}
+                            onChange={handleDescriptionInputChange}
+                            value={description}
+                            name="description" placeholder=" "
+                          >
+                          </textarea>
+                          <span className="custom-input__error">
+                            {descriptionInputUsed && descriptionError}
+                          </span>
                         </label>
                       </div>
                       <div className="questionnaire-coach__checkbox">
                         <label>
-                          <input type="checkbox" value="individual-training" name="individual-training"/>
+                          <input onChange={() => setIsReadyToTrain((prevState) => !prevState)} type="checkbox" value="individual-training" name="individual-training"/>
                           <span className="questionnaire-coach__checkbox-icon">
                             <svg width="9" height="6" aria-hidden="true">
                               <use xlinkHref="#arrow-check"></use>
